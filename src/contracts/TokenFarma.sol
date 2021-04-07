9e82b356b5a5d377c5c349e99720b25086549d53
Filip Domic <filipdomic@gmail.com>
2021-04-07 16:54:10 +0200

    3

--- src/contracts/TokenFarma.sol
+++ src/contracts/TokenFarma.sol
@@ -3,6 +3,5 @@ pragma solidity  ^0.5.0;
 contract TokenFarma{ //Pametni ugovor
  // Cijeli kod ide ovdije
 
-    string public name = "Dapp token farma"
-
+    string public name = "Dapp token farma ";
 }
